268b57e5d2788502b37a10428275052f74d84ef6
Christopher Schulz <c.schulz@accelery.de>
2025-06-12 12:25:36 +0200

    Aktualisiere Middleware-Konfiguration: Aktiviere CORS mit spezifischen Ursprüngen für die lokale Entwicklung und Produktionsumgebung, um die Sicherheit und Flexibilität zu verbessern.

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,27 @@
+// Strapi v4: config/middlewares.js
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'https://gartenprofi-hamburg.de',
+        'https://www.gartenprofi-hamburg.de',
+        'http://localhost:3000', // für lokale Entwicklung
+      ],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
+
+// Alternative: Alle Origins erlauben (nur für Entwicklung!)
+// origin: '*', 
