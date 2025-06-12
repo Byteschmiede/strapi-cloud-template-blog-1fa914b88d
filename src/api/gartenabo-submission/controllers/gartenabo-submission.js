7d9990ff21bafb744d6feb833363799313ebe50a
Christopher Schulz <c.schulz@accelery.de>
2025-06-12 16:10:55 +0200

    Verbessere Fehlerbehandlung in der Middleware: Implementiere try/catch-Blöcke zur besseren Handhabung von Ausnahmen und zur Verbesserung der Stabilität der Anwendung.

---
+++ src/api/gartenabo-submission/controllers/gartenabo-submission.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stadtteil2 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gartenabo-submission.gartenabo-submission');
